0d2774050a72d5f0fbe584d3762196be34f071e6
Charles Lechasseur <shiftingbeard@gmx.com>
2023-08-16 22:18:46 -0400

    Add more than 10 annotations in Rust test

--- rust_tests/fmt_warnings/src/other.rs
+++ rust_tests/fmt_warnings/src/other.rs
@@ -1,4 +1,134 @@
-pub fn not_formatted_propely_either(
+pub fn not_formatted_propely_either1(
+)
+{
+    println!(
+        "What"
+    )
+    ;
+}
+
+
+pub fn not_formatted_propely_either2(
+)
+{
+    println!(
+        "What"
+    )
+    ;
+}
+
+
+pub fn not_formatted_propely_either3(
+)
+{
+    println!(
+        "What"
+    )
+    ;
+}
+
+
+pub fn not_formatted_propely_either4(
+)
+{
+    println!(
+        "What"
+    )
+    ;
+}
+
+
+pub fn not_formatted_propely_either5(
+)
+{
+    println!(
+        "What"
+    )
+    ;
+}
+
+
+pub fn not_formatted_propely_either6(
+)
+{
+    println!(
+        "What"
+    )
+    ;
+}
+
+
+pub fn not_formatted_propely_either7(
+)
+{
+    println!(
+        "What"
+    )
+    ;
+}
+
+
+pub fn not_formatted_propely_either8(
+)
+{
+    println!(
+        "What"
+    )
+    ;
+}
+
+
+pub fn not_formatted_propely_either9(
+)
+{
+    println!(
+        "What"
+    )
+    ;
+}
+
+
+pub fn not_formatted_propely_either10(
+)
+{
+    println!(
+        "What"
+    )
+    ;
+}
+
+
+pub fn not_formatted_propely_either11(
+)
+{
+    println!(
+        "What"
+    )
+    ;
+}
+
+
+pub fn not_formatted_propely_either12(
+)
+{
+    println!(
+        "What"
+    )
+    ;
+}
+
+
+pub fn not_formatted_propely_either13(
+)
+{
+    println!(
+        "What"
+    )
+    ;
+}
+
+
+pub fn not_formatted_propely_either14(
 )
 {
     println!(
